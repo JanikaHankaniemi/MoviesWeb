23095a31f8ca440cd420e7c121919e4bf5e7007b
Hankaniemi Janika <janika.hankaniemi@etteplan.com>
2022-11-01 04:00:42 +0200

    Add links

--- movies-web/src/components/common/MobileDrawer.js
+++ movies-web/src/components/common/MobileDrawer.js
@@ -4,29 +4,27 @@ import {
   IconButton,
   List,
   ListItemButton,
-  ListItemIcon,
   ListItemText,
+  Typography
 } from "@mui/material"
 import { Menu } from "@mui/icons-material";
-
-const MobileDrawer = ({links}) => {
+import { Link } from 'react-router-dom';
+const MobileDrawer = ({ links }) => {
   const [open, setOpen] = useState(false);
   return (
     <>
       <Drawer open={open} onClose={() => setOpen(false)} PaperProps={{ sx: { innerWidth: 50 } }} style={{ background: "#7B9E87" }}>
         <List>
-          {links.map((link, index) =>(
-            <ListItemButton sx={{ paddingLeft: 5, paddingRight: 5 }} key={index} onClick={() => setOpen(false)}>
-            <ListItemIcon>
-                <ListItemText>{ link.title }</ListItemText>
-            </ListItemIcon>
+          {links.map((linkItem, index) => (
+            <ListItemButton sx={{ paddingLeft: 5, paddingRight: 5 }} key={index} onClick={() => setOpen(false)} component={Link} to={linkItem.path}>
+              <ListItemText><Typography variant="button">{linkItem.title}</Typography></ListItemText>
             </ListItemButton>
           ))}
         </List>
       </Drawer>
       <IconButton sx={{ marginLeft: "auto" }} onClick={() => setOpen(!open)}>
         <Menu sx={{ color: 'white' }} />
-        </IconButton>
+      </IconButton>
     </>
   );
 }
