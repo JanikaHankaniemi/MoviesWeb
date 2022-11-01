bfe8307a95381df32757eb73e893da2779ac6379
Hankaniemi Janika <janika.hankaniemi@etteplan.com>
2022-11-01 02:02:28 +0200

    Add stuff

--- movies-web/src/components/movies/AddMovieContainer.js
+++ movies-web/src/components/movies/AddMovieContainer.js
@@ -1,18 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import {
   Typography,
   Grid,
   Card,
-  TextField
+  TextField,
+  MenuItem,
+  Select,
+  InputLabel
 } from '@mui/material';
 import { addMovie } from '../../redux/MoviesSlice';
 import { getGenres } from '../../redux/MoviesSlice';
+import { GetStarOptions } from '../common/Utils'
 
 function AddMovieContainer() {
   const dispatch = useDispatch();
   const { register, handleSubmit, reset } = useForm();
+  const [genreSelection, setGenre] = useState([]);
+
   const genres = useSelector(
     (state) => state.movies.genres);
   const isFetchingGenres = useSelector(
@@ -24,6 +30,12 @@ function AddMovieContainer() {
     } catch (error) { }
   };
 
+  const handleChange = (event) => {
+    const value = event.target.value;
+    console.log("newValue", value)
+    setGenre(value);
+  };
+
   const clearForm = async () => {
     try {
       reset();
@@ -32,7 +44,7 @@ function AddMovieContainer() {
 
     }
   }
-
+  console.log("genreSelection", genreSelection)
   useEffect(() => {
     if (!genres) {
       dispatch(getGenres());
@@ -68,6 +80,79 @@ function AddMovieContainer() {
                   label="Synopsis"
                   variant="outlined"
                   fullWidth />
+              </Grid>
+              <Grid item xs={12} sx={{ padding: '10px' }}>
+                <TextField
+                  id="actors"
+                  name="actors"
+                  {...register("actors")}
+                  label="Actors"
+                  variant="outlined"
+                  fullWidth />
+              </Grid>
+              <Grid item xs={12} sx={{ padding: '10px' }}>
+                <TextField
+                  id="director"
+                  name="director"
+                  {...register("director")}
+                  label="Director"
+                  variant="outlined"
+                  fullWidth />
+              </Grid>
+              <Grid item xs={12} md={4} lg={3} sx={{ padding: '10px' }}>
+                <TextField
+                  id="year"
+                  name="year"
+                  {...register("year")}
+                  label="Year"
+                  variant="outlined"
+                  fullWidth />
+              </Grid>
+              <Grid item xs={12} md={4} lg={3} sx={{ padding: '10px' }}>
+                <TextField
+                  select
+                  id="rating"
+                  name="rating"
+                  {...register("rating")}
+                  label="Rating"
+                  fullWidth
+                  defaultValue=""
+                >
+                  <MenuItem key="nullselect" value="">&nbsp;</MenuItem>
+                  {GetStarOptions()}
+                </TextField>
+              </Grid>
+              <Grid item xs={12} md={4} lg={3} sx={{ padding: '10px' }}>
+                <TextField
+                  select
+                  id="ageLimit"
+                  name="ageLimit"
+                  {...register("ageLimit")}
+                  label="Age Limit"
+                  fullWidth
+                  defaultValue=""
+                >
+                  <MenuItem key="nullselect" value="">&nbsp;</MenuItem>
+                  {[...Array(19).keys()].map(age => <MenuItem key={`age${age}`} value={age}>{age}</MenuItem>)}
+                </TextField>
+              </Grid>
+              <Grid item xs={12} md={4} lg={3} sx={{ padding: '10px' }}>
+                {!isFetchingGenres && genres && (
+                  <>
+                    <Select multiple
+                      id="genre"
+                      name="genre"
+                      fullWidth
+                      label="test"
+                      value={genreSelection}
+                      onChange={handleChange}
+                      {...register("genre")}
+                    >
+                      <MenuItem key="nullselect" value="">&nbsp;</MenuItem>
+                      {genres.map(genre => <MenuItem key={`genre${genre.name}`} value={genre.name}>{genre.name}</MenuItem>)}
+                    </Select>
+                  </>
+                )}
               </Grid>
             </Grid>
           </form>
