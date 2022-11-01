f9a79b3cf7bf1d86665ba702821dd4a30be7db18
Hankaniemi Janika <janika.hankaniemi@etteplan.com>
2022-11-01 15:11:34 +0200

    Remove import

--- movies-web/src/components/movies/AddMovieContainer.js
+++ movies-web/src/components/movies/AddMovieContainer.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
-import { ErrorMessage } from '@hookform/error-message';
 import {
   Typography,
   Grid,
